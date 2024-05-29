d4149758a74c97ac8b8f50ad2d386225b605b78c
sparshkr <sparshkumar910@gmail.com>
2024-05-29 23:58:07 +0530

    Update

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -1,9 +1,10 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const PRIVATE_KEY = "YOUR_PRIVATE_KEY";
-const RPC_URL = "https://rpc.ankr.com/polygon_mumbai";
+const PRIVATE_KEY =
+  "6d90411b0d9cce9f4ba9f0d84f340d5f8d6408ef03ff030d01f4c9780217b183";
+// const RPC_URL = "https://rpc.ankr.com/polygon_mumbai";
 module.exports = {
-  defaultNetwork: "polygon_mumbai",
+  defaultNetwork: "sepolia",
   networks: {
     hardhat: {
       chainId: 80001,
@@ -11,6 +12,12 @@ module.exports = {
     polygon_mumbai: {
       url: "https://rpc.ankr.com/polygon_mumbai",
       accounts: [`0x${PRIVATE_KEY}`],
+    },
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/bOY9sPwzLLIyHZjwR2QQFX9AtruNz5Tz",
+      accounts: [
+        "6d90411b0d9cce9f4ba9f0d84f340d5f8d6408ef03ff030d01f4c9780217b183",
+      ],
     },
   },
   solidity: {
